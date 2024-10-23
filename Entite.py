50d5595cda0f8f8c4f311b3b79b682f07322848c
HctrNvt <169070816+HctrNvt@users.noreply.github.com>
2024-10-24 00:27:09 +0200

    Système de données

--- Entite.py
+++ Entite.py
@@ -1,17 +1,45 @@
 class SaveAble:
-    self.attributs = []
     def __init__(self) -> None:
+        self.attributs = {}
         pass
 
-class Entite:
-    def __init__(self,nom):
-        self.nom = nom
-    def getNom(self):
-        return self.nom
+class Entite(SaveAble):
+    def __init__(self,id):
+        super().__init__()
+        self.attributs.update({"id":id})
+    def getId(self):
+        return self.attributs["id"]
+
+class Effet(Entite):
+    def __init__(self,opérateur,bonus):
+        super().__init__("Effet")
+        if opérateur == "+":
+            self.attributs.update({"+":bonus})
+        elif opérateur == "*":
+            self.attributs.update({"*":bonus})
+
+# C'est un bonus que le joueur peut acheter 
+# Il s'applique quand le joueur clic, PAS passif
+class BonusStatique(Entite):
+    def __init__(self,id,prix,effet:Effet):
+        super().__init__(id)
+        self.attributs.update({"prix":prix})
+        self.attributs.update({"effet":effet.attributs})
 
 class Joueur(Entite):
-    def __init__(self,nom,score):
-        super().__init__(nom)
-        self.score = score
+    def __init__(self,id,score):
+        super().__init__(id)
+        self.attributs.update({"score":score})
+        self.attributs.update({"bonus":{}})
+
+    def ajouterBonus(self,bonus):
+        """Ajoute un bonus au joueur. Si le bonus est déjà présent, ajoute +1 à son compteur.
+        Sinon, l'ajoute avec un compteur de 1."""
+        if bonus.getId() in self.attributs["bonus"]:
+            self.attributs["bonus"][bonus.getId()] += 1
+        else:
+            self.attributs["bonus"][bonus.getId()] = 1
+    def clic(self):
+        self.ajouterScore(1)
     def ajouterScore(self,score):
-        self.score += score
+        self.attributs["score"] += score
--- game.py
+++ game.py
@@ -1,12 +1,24 @@
 import json
-from entite import Joueur
+from entite import Joueur,BonusStatique,EffetStatique
 class Game:
     def __init__(self,sauvegarde):
         self.load(sauvegarde)
-    def load(self,sauvegarde):
-        data = json.loads(sauvegarde)
-        print("sauvegarde chargee")
-        self.joueur = Joueur(data["joueur"]["nom"],data["joueur"]["score"])
+    def registerBonus(self):
+        self.bonus = []
+        self.bonus.append(BonusStatique("Nouvelle souris",100,EffetStatique(1)))
+        self.bonus.append(BonusStatique("Amis qui cliquent",850,EffetStatique(5)))
+        self.bonus.append(BonusStatique("Souris en or",10000,EffetStatique(25)))
         
+    def clique(self):
+        self.joueur.clic()
+    def load(self,sauvegarde):
+        # On enregistre toute les informations qui ne sont pas dans une save
+        self.registerBonus()
+        
+        # On charge la sauvegarde
+        if sauvegarde != None:
+            data = json.loads(sauvegarde)
+            print("sauvegarde chargee")
+            self.joueur = Joueur(data["joueur"]["nom"],data["joueur"]["score"])
     def save(self):
         pass
